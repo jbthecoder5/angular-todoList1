71377596a117835c170218f4ac434d766ecdc3b2
Joseph Borne <josephpbourne@outlook.com>
2023-02-10 21:21:08 -0800

    small change to the todo component typescript file

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  title = 'Todo';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
