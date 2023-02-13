affd8771bf45b5f9a4e7e4f659f1a8bfde43168f
Joseph Borne <josephpbourne@outlook.com>
2023-02-12 19:24:27 -0800

    small change

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
-  title = 'Todo';
+  todo = [];
 
   constructor() {}
 
